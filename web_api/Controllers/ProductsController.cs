ddc86da0632336aea45c7615dd92b4d3e097cc3e
fangxin <13839870291@users.noreply.github.com>
2019-06-27 13:30:26 +0800

    添加项目文件。

---
+++ web_api/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using web_api.Filter;
+using web_api.Models;
+
+namespace web_api.Controllers
+{
+    [BasicAuthenticationAttribute]
+    public class ProductsController : ApiController
+    {
+  
+        Product[] products = new Product[]
+           {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+           };
+
+ 
+        [HttpGet, AllowAnonymous]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+ 
+        [HttpGet, AllowAnonymous]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [HttpGet]
+        public IHttpActionResult GetPageRow(int limit, int offset)
+        {
+            var lstRes = new List<Product>();
+
+            //实际项目中，通过后台取到集合赋值给lstRes变量。这里只是测试。
+            lstRes.Add(new Product() { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M });
+            lstRes.Add(new Product() { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M });
+
+            var oData = new { total = lstRes.Count, rows = lstRes.Skip(offset).Take(limit).ToList() };
+            return new PageResult(oData, Request);
+        }
+
+    }
+}
---
+++ web_api/Filter/BasicAuthenticationAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.WebPages;
+
+namespace web_api.Filter
+{
+    public class BasicAuthenticationAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// 检查用户是否有该Action执行的操作权限
+        /// </summary>
+        /// <param name="actionContext"></param>
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            bool isRquired = false;
+
+            try
+            {
+                //获取刚才在webconfig中的添加的权限开关
+                isRquired = WebConfigurationManager.AppSettings["WebApiAuthFlag"].ToString().AsBool();
+            }
+            catch (Exception ex)
+            {
+                //抛出异常
+            }
+
+            //如果开启...
+            if (isRquired)
+            {
+                bool isLogin = false;
+
+                //用户登录验证
+                //代码.....
+                //代码.....
+                //代码.....
+                //isLogin = true or false;
+
+                if (isLogin)
+                {
+                    //如果已经登录，则跳过验证
+                    base.OnActionExecuting(actionContext);
+                }
+                else
+                {
+                    //如果请求Header不包含ticket，则判断是否是匿名调用
+                    var attr = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().OfType<AllowAnonymousAttribute>();
+                    bool isAnonymous = attr.Any(a => a is AllowAnonymousAttribute);
+
+                    //是匿名用户，则继续执行；非匿名用户，抛出“未授权访问”信息 ( 抛出401 )
+                    if (isAnonymous)
+                        base.OnActionExecuting(actionContext);
+                    else
+                        actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                }
+            }
+        }
+    }
+}
