0ab4c3bcb58e5d23d6632f939a86a56349b53454
fangxin <13839870291@users.noreply.github.com>
2019-07-05 11:47:45 +0800

    还原

--- web_api/App_Start/WebApiConfig.cs
+++ web_api/App_Start/WebApiConfig.cs
@@ -19,7 +19,7 @@ namespace web_api
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { id = RouteParameter.Optional } 
             );
         }
     }
