9ca6ef7389e02e5b3d81773fd87085513b09cc4e
fangxin <13839870291@users.noreply.github.com>
2019-07-12 11:54:01 +0800

    微信小程序

--- web_api/Controllers/ArticleController.cs
+++ web_api/Controllers/ArticleController.cs
@@ -12,18 +12,31 @@ namespace web_api.Controllers
 
 
 
-        //[HttpGet]
-        //public IHttpActionResult GetPageRow(int limit, int offset)
-        //{
-        //    //var lstRes = new List<Product>();
+        [HttpGet, AllowAnonymous]
+        public IEnumerable<tArticle> GetAllArticle()
+        {
+            using (xdwlEntities dbContext = new xdwlEntities())
+            {
+                List<tArticle> list = dbContext.tArticle.Where(u => u.ArtTyID == (int)5).ToList();
+                return list;
+            }
+        }
 
-        //    ////实际项目中，通过后台取到集合赋值给lstRes变量。这里只是测试。
-        //    //lstRes.Add(new Product() { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M });
-        //    //lstRes.Add(new Product() { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M });
 
-        //    //var oData = new { total = lstRes.Count, rows = lstRes.Skip(offset).Take(limit).ToList() };
-        //    //return new PageResult(oData, Request);
-        //}
+        [HttpGet, AllowAnonymous]
+        public IHttpActionResult GetArticle(int id)
+        {
+            using (xdwlEntities dbContext = new xdwlEntities())
+            {
+                tArticle contact = dbContext.tArticle.Find(id);
+
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+                return Ok(contact);
+            }
+        }
     }
 
 }
